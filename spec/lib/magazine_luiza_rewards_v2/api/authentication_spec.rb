b885e7834959fdd3036de848097a097e616e7e28
André Rodrigues <andrerpbts@gmail.com>
2024-06-18 18:16:40 -0300

    Fix rubocop offenses

--- spec/lib/magazine_luiza_rewards_v2/api/authentication_spec.rb
+++ spec/lib/magazine_luiza_rewards_v2/api/authentication_spec.rb
@@ -12,25 +12,25 @@ RSpec.describe MagazineLuizaRewardsV2::Api::Authentication do
     let(:password) { 'pass123' }
     let(:expected_expires_in) { 8979 }
     let(:expected_token) do
-      'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ0TGxHNW9iY1JKaWpFTHFsNnFCdmtEbndDdzRkZHFz' \
-        'MG5MVWFmMk1udkUwIn0.eyJqdGkiOiI1ODk4NGZhMC05YTFjLTQyYjktYmRmZi1lZjY0MmNiYWFjMDciLCJleHAiOj' \
-        'E2OTU3NjQzMjksIm5iZiI6MCwiaWF0IjoxNjk1NzI4MzI5LCJpc3MiOiJodHRwczovL2tleWNsb2FrLXN0YWdpbmcu' \
-        'bHVpemFsYWJzLmNvbS9hdXRoL3JlYWxtcy9CMkIiLCJhdWQiOlsiZnVsbGNvbW1lcmNlLWFwaSIsImFjY291bnQiXS' \
-        'wic3ViIjoiY2JlMTRkYmQtMjkyYS00NzIzLWE4MTEtNGUzOWIyMDI5M2IxIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoi' \
-        'cHJvZ3JhbWFzLXBhcmNlaXJvLXRlc3RlLW1hZ2FsdS1wbGF0Zm9ybS1hcGkiLCJhdXRoX3RpbWUiOjAsInNlc3Npb2' \
-        '5fc3RhdGUiOiI4YjM4Mzc4Ny1lZTVlLTRmYzctYWYwNC0wN2Q3MjA0MDBlZTIiLCJhY3IiOiIxIiwicmVhbG1fYWNj' \
-        'ZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2' \
-        'xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2Nv' \
-        'cGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SWQiOiJwcm9ncmFtYXMtcGFyY2Vpcm8tdGVzdGUtbWFnYWx1LXBsYX' \
-        'Rmb3JtLWFwaSIsInBhcnRuZXJfaWQiOiIxNjg1MSIsImNsaWVudEhvc3QiOiIxNDguNTkuNzIuMjU0IiwiZW1haWxf' \
-        'dmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtcHJvZ3JhbWFzLXBhcm' \
-        'NlaXJvLXRlc3RlLW1hZ2FsdS1wbGF0Zm9ybS1hcGkiLCJjaGFubmVsX2lkIjoiMyIsImNsaWVudEFkZHJlc3MiOiIx' \
-        'NDguNTkuNzIuMjU0IiwiZW1haWwiOiJzZXJ2aWNlLWFjY291bnQtcHJvZ3JhbWFzLXBhcmNlaXJvLXRlc3RlLW1hZ2' \
-        'FsdS1wbGF0Zm9ybS1hcGlAcGxhY2Vob2xkZXIub3JnIn0.iO75kDgr6q2EBpbZANpghKdvb_bXz_lroQ05SM4mLysg' \
-        'wRsAkuZqy6HnvNGIGbxTUCLb_kfl953qdNA-vx_Qnwc8EpOOkX7JWSFGbN95gwkZ7Cn0wwbryYrtmC-jqoKhI9eiPR' \
-        'ipdzexBu1fK0mmBh3Gp5tlB1m6nD1BBvwCpPwY4sf3Lblmh_d6eDRntMPYlyc4X0SOS3jA2a1gJXWZC0q3n0ktIFti' \
-        '02wsGc2W64Yii958fRKXHJQosHgrKBAhdBVnM4jAsRjr8ieWz7OtIeu4q9jevBJJLWS9dFlfi88zrsjItsl2Gwlemo' \
-        '9_Gi64UfH87Heug8S8SMpjef4udQ'
+      'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ0TGxHNW9iY1JKaWpFTHFsNnFCdmtEbndDdzRkZH' \
+        'FzMG5MVWFmMk1udkUwIn0.eyJqdGkiOiI1ODk4NGZhMC05YTFjLTQyYjktYmRmZi1lZjY0MmNiYWFjMDciLCJleH' \
+        'AiOjE2OTU3NjQzMjksIm5iZiI6MCwiaWF0IjoxNjk1NzI4MzI5LCJpc3MiOiJodHRwczovL2tleWNsb2FrLXN0YW' \
+        'dpbmcubHVpemFsYWJzLmNvbS9hdXRoL3JlYWxtcy9CMkIiLCJhdWQiOlsiZnVsbGNvbW1lcmNlLWFwaSIsImFjY2' \
+        '91bnQiXSwic3ViIjoiY2JlMTRkYmQtMjkyYS00NzIzLWE4MTEtNGUzOWIyMDI5M2IxIiwidHlwIjoiQmVhcmVyIi' \
+        'wiYXpwIjoicHJvZ3JhbWFzLXBhcmNlaXJvLXRlc3RlLW1hZ2FsdS1wbGF0Zm9ybS1hcGkiLCJhdXRoX3RpbWUiOj' \
+        'AsInNlc3Npb25fc3RhdGUiOiI4YjM4Mzc4Ny1lZTVlLTRmYzctYWYwNC0wN2Q3MjA0MDBlZTIiLCJhY3IiOiIxIi' \
+        'wicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYW' \
+        'Njb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm' \
+        '9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SWQiOiJwcm9ncmFtYXMtcGFyY2Vpcm8tdG' \
+        'VzdGUtbWFnYWx1LXBsYXRmb3JtLWFwaSIsInBhcnRuZXJfaWQiOiIxNjg1MSIsImNsaWVudEhvc3QiOiIxNDguNT' \
+        'kuNzIuMjU0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY2' \
+        '91bnQtcHJvZ3JhbWFzLXBhcmNlaXJvLXRlc3RlLW1hZ2FsdS1wbGF0Zm9ybS1hcGkiLCJjaGFubmVsX2lkIjoiMy' \
+        'IsImNsaWVudEFkZHJlc3MiOiIxNDguNTkuNzIuMjU0IiwiZW1haWwiOiJzZXJ2aWNlLWFjY291bnQtcHJvZ3JhbW' \
+        'FzLXBhcmNlaXJvLXRlc3RlLW1hZ2FsdS1wbGF0Zm9ybS1hcGlAcGxhY2Vob2xkZXIub3JnIn0.iO75kDgr6q2EBp' \
+        'bZANpghKdvb_bXz_lroQ05SM4mLysgwRsAkuZqy6HnvNGIGbxTUCLb_kfl953qdNA-vx_Qnwc8EpOOkX7JWSFGbN' \
+        '95gwkZ7Cn0wwbryYrtmC-jqoKhI9eiPRipdzexBu1fK0mmBh3Gp5tlB1m6nD1BBvwCpPwY4sf3Lblmh_d6eDRntM' \
+        'PYlyc4X0SOS3jA2a1gJXWZC0q3n0ktIFti02wsGc2W64Yii958fRKXHJQosHgrKBAhdBVnM4jAsRjr8ieWz7OtIe' \
+        'u4q9jevBJJLWS9dFlfi88zrsjItsl2Gwlemo9_Gi64UfH87Heug8S8SMpjef4udQ'
     end
 
     context 'when successful', vcr: { cassette_name: 'success_authentication_request' } do
